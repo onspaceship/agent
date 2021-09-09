76c89ce0fa2a4b3e5996ff19f60e8a9a0d2f9f7f
Tim Dorr <git@timdorr.com>
2021-09-09 17:22:45 -0400

    Handle delivery events and update the deploy image

--- pkg/socket/handle_delivery.go
+++ pkg/socket/handle_delivery.go
@@ -1,9 +1,14 @@
 package socket
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"time"
 
 	"github.com/apex/log"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 )
 
 const (
@@ -25,7 +30,35 @@ func handleDelivery(jsonPayload []byte, socket *socket) {
 		return
 	}
 
-	log.WithField("payload", payload).Info("Handling delivery")
+	log.WithField("app_id", payload.AppId).WithField("delivery_id", payload.DeliveryId).Info("Handling delivery")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	deployments, err := socket.client.AppsV1().Deployments("").List(ctx, metav1.ListOptions{
+		LabelSelector: labels.Set{AppIdAnnotation: payload.AppId}.AsSelector().String(),
+	})
+	if err != nil {
+		log.WithError(err).Fatal("Could not get Kubernetes deployment for Agent")
+	}
+
+	for _, deployment := range deployments.Items {
+		logline := log.WithField("deployment", fmt.Sprintf("%s/%s", deployment.Namespace, deployment.Name))
+		logline.Info("Updating image")
+
+		for i, container := range deployment.Spec.Template.Spec.Containers {
+			container.Image = payload.ImageURI
+			deployment.Spec.Template.Spec.Containers[i] = container
+		}
+
+		_, err = socket.client.AppsV1().Deployments(deployment.Namespace).Update(ctx, &deployment, metav1.UpdateOptions{})
+		if err != nil {
+			logline.WithError(err).Info("Could not update image")
+			return
+		} else {
+			logline.Info("Image updated!")
+		}
+	}
 }
 
 func init() {
