7e56abc58b1c063fb9c50d38054fbebd98400a42
Tim Dorr <git@timdorr.com>
2021-11-02 21:10:24 -0400

    Forgot to include the new annotations

--- pkg/config/types.go
+++ pkg/config/types.go
@@ -1,8 +1,11 @@
 package config
 
 const (
-	AppIdLabel           = "onspaceship.com/app-id"
-	DeliveryIdAnnotation = "onspaceship.com/delivery-id"
-	AppHandleAnnotation  = "onspaceship.com/app-handle"
-	TeamHandleAnnotation = "onspaceship.com/team-handle"
+	AppIdLabel                 = "onspaceship.com/app-id"
+	DeliveryIdAnnotation       = "onspaceship.com/delivery-id"
+	DeliveryRevisionAnnotation = "onspaceship.com/delivery-revision"
+	AppHandleAnnotation        = "onspaceship.com/app-handle"
+	TeamHandleAnnotation       = "onspaceship.com/team-handle"
+
+	KubernetesRevisionAnnotation = "deployment.kubernetes.io/revision"
 )
